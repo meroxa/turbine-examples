161714f5b67ef28e48ab29744ed56b47b3cf9f54
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2023-02-28 10:26:11 -0800

    merge in existing example repos

---
+++ javascript/users-demo/meroxa_user_demo_data_app/helpers.js
@@ -0,0 +1,19 @@
+function formatPhone() {
+    // Custom logic to format phone numbers
+    return
+}
+
+function googleMapsLookup() {
+    // Api call
+    return
+}
+
+function generateAddressObject() {
+    // Other helper function
+}
+
+module.exports = {
+    formatPhone,
+    googleMapsLookup,
+    generateAddressObject,
+}
---
+++ python/README.md
@@ -0,0 +1,9 @@
+# turbine-py-examples
+
+## Dependencies
+* Python > v3.5
+* [turbine-py](https://github.com/meroxa/turbine-py)
+
+### License
+
+Licensed under the [MIT License](./LICENSE.md).
