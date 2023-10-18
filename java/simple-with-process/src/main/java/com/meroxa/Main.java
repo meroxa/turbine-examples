095af51377c6fbe37d0f3ae0e4e7f1205e20476c
Raúl Barroso <ra.barroso@gmail.com>
2023-10-18 18:04:22 +0200

    update: example

--- java/simple-with-process/src/main/java/com/meroxa/Main.java
+++ java/simple-with-process/src/main/java/com/meroxa/Main.java
@@ -1,18 +1,16 @@
 package com.meroxa;
 
-import com.meroxa.turbine.Runner;
 import com.meroxa.turbine.Turbine;
 import com.meroxa.turbine.TurbineApp;
 import com.meroxa.turbine.TurbineRecord;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
 
 import static java.time.LocalDateTime.now;
 
+@ApplicationScoped
 public class Main implements TurbineApp {
-    public static void main(String[] args) {
-        Runner.start(new Main());
-    }
 
     @Override
     public void setup(Turbine turbine) {
